226aa21a50f9ffc321bb79fb1956ecde34f9450a
kabi239 <kavyasharma239@gmail.com>
2024-12-25 02:01:49 +0530

    Implemented Http Basic Auth

---
+++ securitydemo/src/main/java/com/example/securitydemo/securityConfig.java
@@ -0,0 +1,33 @@
+package com.example.securitydemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+//Tells spring that this provides configuration to tha application context
+@EnableWebSecurity
+//-This annotation tells spring boot to enable web security features in the application.
+//-Also gives us the liberty, where using this particular class we are able
+// to customize the security configs.
+public class securityConfig {
+    @Bean
+    //this make sure that the below method is available as bean provider
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> {
+            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
+        });
+        //the above line tells that any coming request gets authenticated by default.
+
+        //http.formLogin(Customizer.withDefaults()); -> this is for form based login
+        http.httpBasic(Customizer.withDefaults());
+        // implementing http basic auth
+
+        return (SecurityFilterChain)http.build();
+        //returning the object of type security filter chain
+    }
+}
