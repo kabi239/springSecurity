d26477369e114c8b59dd8340a7ae294dcbf4b1d0
kabi239 <kavyasharma239@gmail.com>
2024-12-27 01:01:09 +0530

    Implemented session management policy

--- securitydemo/src/main/java/com/example/securitydemo/securityConfig.java
+++ securitydemo/src/main/java/com/example/securitydemo/securityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,6 +23,10 @@ public class securityConfig {
             ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
         });
         //the above line tells that any coming request gets authenticated by default.
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        //Configures session management policy to be stateless,
+        // meaning the server will not store session information.
 
         //http.formLogin(Customizer.withDefaults()); -> this is for form based login
         http.httpBasic(Customizer.withDefaults());
