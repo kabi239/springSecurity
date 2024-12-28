4c6108187b92e1a4c41bb4eb34a27ebd2f11c4e0
kabi239 <kavyasharma239@gmail.com>
2024-12-29 01:42:38 +0530

    Configured H2 Database

--- securitydemo/src/main/java/com/example/securitydemo/securityConfig.java
+++ securitydemo/src/main/java/com/example/securitydemo/securityConfig.java
@@ -6,7 +6,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -31,7 +33,9 @@ public class securityConfig {
     //this make sure that the below method is available as bean provider
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> {
-            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
+            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests
+                    .requestMatchers("/h2-console/**").permitAll()
+                    .anyRequest()).authenticated();
         });
         //the above line tells that any coming request gets authenticated by default.
         http.sessionManagement(session ->
@@ -43,8 +47,10 @@ public class securityConfig {
         //http.formLogin(Customizer.withDefaults()); -> this is for form based login
         http.httpBasic(Customizer.withDefaults());
         // implementing http basic auth
-
-        return (SecurityFilterChain)http.build();
+        http.headers(headers ->
+                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
         //returning the object of type security filter chain
     }
 
